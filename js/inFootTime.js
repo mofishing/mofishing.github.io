ac2ed9efae737f2e0c13f86cf1328ada12fd6dfb
mofishing <mo_fishing@163.com>
2025-06-22 12:04:03 +0800

    Site updated: 2025-06-22 12:04:02

--- js/inFootTime.js
+++ js/inFootTime.js
@@ -61,7 +61,7 @@
         if (String(snum).length === 1) {
             snum = "0" + snum;
         }
-        document.getElementById("timeDate").innerHTML = "摸鱼猫的小屋已经开业&nbsp" + ynum + "&nbsp年&nbsp" + dnum + "&nbsp天";
+        document.getElementById("timeDate").innerHTML = "摸鱼猫的小屋已经开业&nbsp" + ynum + "&nbsp年" + dnum + "&nbsp天";
         document.getElementById("times").innerHTML = hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒";
     }
     setInterval(update, 1000);
